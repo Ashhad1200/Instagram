47909d4babbaed491e627c87c59707c599fc660b
Syed Ashhad <syedashhad17@gmail.com>
2024-03-28 12:20:17 +0500

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./Pages/HomePage/HomePage"
-import AuthPage from "./Pages/AuthPage/AuthPage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
+import AuthPage from "./Pages/AuthPage/AuthPage";
+import PageLayout from "./Layouts/PageLayout/PageLayout";
 
 function App() {
-
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/auth' element={<AuthPage />} />
-    </Routes>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </PageLayout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
